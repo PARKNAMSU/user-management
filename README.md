20d98e56ccf32f64fa6d9abdf32759c854a64044
namsupark <skatn7979@gmail.com>
2024-02-26 22:16:03 +0900

    fiber init

---
+++ README.md
@@ -0,0 +1,10 @@
+# User Management System
+
+## 초기세팅
+```
+# go project initialize
+go mod init github.com/PARKNAMSU/user-management
+
+# fiber install
+go get github.com/gofiber/fiber/v2
+```
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+var (
+	_ = godotenv.Load()
+)
+
+var (
+	port = fmt.Sprintf(":%s", os.Getenv("PORT"))
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Next()
+	})
+
+	app.Listen(port)
+}
